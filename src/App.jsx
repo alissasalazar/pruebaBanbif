c8e953b458ca3f47dba6f13ceba341bc059ef3d0
alissasalazar <salazargamarraa@gmail.com>
2023-06-01 18:22:15 -0500

    refactor:refactor code for correct brand image

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import { BsChevronLeft } from "react-icons/bs";
-import logo from "../src/img/tarjeta.jpeg"
+import logo from "../src/img/logoBanbif.jpeg"
 
 function App() {
   return (
